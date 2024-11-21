8c052ea7cab971321abf9b9aecf9302b84ee9612
BenTelega <72995848+BenTelega@users.noreply.github.com>
2024-11-21 13:39:01 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -83,7 +83,7 @@ cartFurtherButton.addEventListener("click", () => {
 });
 
 async function loadItems() {
-  const response = await fetch("/static/foodItems.json");
+  const response = await fetch("static/foodItems.json");
   const items = await response.json();
 
   items.forEach((item, index) => {
